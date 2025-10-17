0c16bc12ddaffb404615c0b7aaececfee45cc096
kembel0116@gmail.com <kembel0116@gmail.com>
2025-10-17 16:23:36 +0900

    .

--- sac.py
+++ sac.py
@@ -186,3 +186,126 @@ class ActorClass(nn.Module):
       alpha_loss.backward()
       self.log_alpha_optimizer.step()
 
+# Critic
+class CriticClass(nn.Module):
+    def __init__(self,
+                name      = "critic",
+                obs_dim   = 75,
+                a_dim     = 8,
+                h_dims    = [256,256],
+                out_dim   = 1,
+                lr_critic = 0.0003,
+                device    = None) -> None:
+        """
+            Initialize Critic
+        """
+        super(CriticClass, self).__init__()
+        # Initialize
+        self.name      = name
+        self.obs_dim   = obs_dim
+        self.a_dim     = a_dim
+        self.h_dims    = h_dims
+        self.out_dim   = out_dim
+        self.lr_critic = lr_critic
+        self.device    = device
+        self.init_layers()
+        self.init_params()
+        # Set optimizer
+        self.critic_optimizer = optim.Adam(self.parameters(),lr=self.lr_critic)
+
+    def init_layers(self):
+        """
+            Initialize layers
+        """
+        self.layers = {}
+        h_dim_prev = self.h_dims[0]
+        for h_idx, h_dim in enumerate(self.h_dims):
+            if h_idx == 0:
+                self.layers['obs']      = nn.Linear(self.obs_dim, int(self.h_dims[0]/2))
+                self.layers['obs_relu'] = nn.ReLU()
+                self.layers['act']      = nn.Linear(self.a_dim, int(self.h_dims[0]/2))
+                self.layers['act_relu'] = nn.ReLU()
+            else:
+                self.layers['mlp_{}'.format(h_idx)] = nn.Linear(h_dim_prev, h_dim)
+                self.layers['relu_{}'.format(h_idx)] = nn.ReLU()
+            h_dim_prev = h_dim
+        self.layers['out'] = nn.Linear(h_dim_prev, self.out_dim)
+
+        # Accumulate layers weights
+        self.param_dict = {}
+        for key in self.layers.keys():
+            layer = self.layers[key]
+            if isinstance(layer,nn.Linear):
+                self.param_dict[key+'_w'] = layer.weight
+                self.param_dict[key+'_b'] = layer.bias
+        self.parameters = nn.ParameterDict(self.param_dict)
+
+    def init_params(self):
+        """
+            Initialize parameters
+        """
+        for key in self.layers.keys():
+            layer = self.layers[key]
+            if isinstance(layer,nn.Linear):
+                nn.init.normal_(layer.weight,mean=0.0,std=0.01)
+                nn.init.zeros_(layer.bias)
+            elif isinstance(layer,nn.BatchNorm2d):
+                nn.init.constant_(layer.weight,1.0)
+                nn.init.constant_(layer.bias,0.0)
+            elif isinstance(layer,nn.Conv2d):
+                nn.init.kaiming_normal_(layer.weight)
+                nn.init.zeros_(layer.bias)
+                
+    def forward(self,
+                x,
+                a):
+        x = x.to(self.device)
+        a = a.to(self.device)
+        for h_idx, _ in enumerate(self.h_dims):
+            if h_idx == 0:
+                x = self.layers['obs_relu'](self.layers['obs'](x))
+                a = self.layers['act_relu'](self.layers['act'](a))
+                cat = torch.cat([x,a], dim=1)
+            else:
+                q = self.layers['relu_{}'.format(h_idx)](self.layers['mlp_{}'.format(h_idx)](cat))
+        q = self.layers['out'](q)
+        return q
+    
+    def train(self,
+              target,
+              mini_batch):
+        """
+            Train
+        """
+        s, a, r, s_prime, done = mini_batch
+        critic_loss = F.smooth_l1_loss(self.forward(s,a), target)
+        self.critic_optimizer.zero_grad()
+        critic_loss.mean().backward()
+        self.critic_optimizer.step()
+        
+    def soft_update(self, tau, net_target):
+        """
+            Soft update of Critic
+        """
+        for param_target, param in zip(net_target.parameters(), self.parameters()):
+            param_target.data.copy_(param_target.data * (1.0 - tau) + param.data * tau)
+            
+# Bellman backup operator
+def get_target(pi, q1, q2, gamma, mini_batch, device):
+    q1 = q1.to(device)
+    q2 = q2.to(device)
+    pi = pi.to(device)
+    s, a, r, s_prime, done = mini_batch
+    with torch.no_grad():
+        # We use the action the current 'pi' making SAC off-policy
+        a_prime, log_prob = pi(s_prime)
+        entropy = -pi.log_alpha.exp() * log_prob # incentivize exploration
+        
+        # Use the minimum Q among q1 and q2 (to avoid over-confident issue)
+        q1_val, q2_val = q1(s_prime,a_prime), q2(s_prime,a_prime)
+        q = torch.cat([q1_val, q2_val], dim=1)
+        min_q = torch.min(q, 1, keepdim=True)[0]
+        
+        # Get target using Bellman backup operator
+        target = r + gamma * done * (min_q + entropy.mean()) # Eg. (3)
+    return target 
