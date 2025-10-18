8aa0fc6108f2421dab1fb7fef8aba5b0826127f0
kembel0116@gmail.com <kembel0116@gmail.com>
2025-10-18 14:47:33 +0900

    sumtree class

--- src/models/rl_model.py
+++ src/models/rl_model.py
@@ -8,62 +8,74 @@ import torch.optim as optim
 from torch.distributions import Normal
 
 def list2torch(x, device):
-  """
-  List to torch
-  """
-  return torch.tensor(np.array(x), dtype = torch.float).to(device)
+    """
+    List to torch
+    """
+    return torch.tensor(np.array(x), dtype = torch.float).to(device)
 
-#Replay Buffer
-class ReplayBufferClass():
-  def __init__(self, buffer_limit=100000, device='cpu') -> None:
+#PER(Prioritized Experience Replay)
+class Sumtree:
     """
-    Initialize Buffer
+    algorithm for PER to sample faster, more efficient
     """
-    self.buffer = collections.deque(maxlen=buffer_limit)
-    self.device = device
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.tree = np.zeros(2 * capacity - 1)
+        self.data = np.zeros(capacity, dtype = object)
+        self.n_entries = 0
+        self.write = 0
+
+    def _propagate(self, idx, change):
+        parent = (idx - 1)//2
+        self.tree[parent] += change
+        if parent != 0:
+            self._propagate(parent, change)
     
-  def size(self):
-    """
-    Get buffer size
-    """
-    return len(self.buffer)
-  
-  def clear(self):
-    """
-    Clear buffer
-    """
-    self.buffer.clear()
-
-  def put(self, item):
-    """"
-    Put item in buffer
-    """
-    self.buffer.append(item)
-
-  def put_mini_batch(self, mini_batch):
-    """
-    Put mini batch in buffer
-    """
-    for transition in mini_batch:
-      self.put(transition)
-
-  def sample(self, n):
-    """
-    Sampling 
-    """
-    mini_batch = random.sample(self.buffer, n)
-    s_list, a_list, r_list, s_prime_list, done_mask_list = [], [], [], [], []
-    for transition in mini_batch:
-      s, a, r, s_prime, done_mask = transition
-      s_list.append(s)
-      a_list.append(a)
-      r_list.append([r])
-      s_prime_list.append(s_prime)
-      done_mask = 0.0 if done_mask else 1.0
-      done_mask_list.append([done_mask])
+    def _retrieve(self, idx, s):
+        left = 2 * idx + 1
+        right = left + 1
+        if left >= len(self.tree):
+            return idx
+        if s <= self.tree[left]:
+            return self._retrieve(left, s)
+        else:
+            return self._retrieve(right, s - self.tree[left])
+        
+    def total(self):
+        return self.tree[0]
     
-    return list2torch(s_list, self.device), list2torch(a_list, self.device), list2torch(r_list, self.device), list2torch(s_prime_list, self.device), list2torch(done_mask_list, self.device)
-  
+    def update(self, idx, p):
+        change = p - self.tree[idx]
+        self.tree[idx] = p
+        self._propagate(idx, change)
+    
+    def add(self, p, data):
+        idx = self.write + self.capacity - 1
+        self.data[self.write] = data
+        self.update(idx, p)
+        self.write += 1
+        if self.write >= self.capacity:
+            self.write = 0
+        if self.n_entries < self.capacity:
+            self.n_entries += 1
+
+    def get(self, s):
+        idx = self._retrieve(0, s)
+        dataIdx = idx - self.capacity + 1
+        return (idx, self.tree[idx], self.data[dataIdx])
+    
+class PERBufferClass:
+    """
+    Prioritized Experience Replay (PER) Buffer for efficient sampling
+    """
+    def __init__(self, 
+                buffer_limit = 100000, 
+                device = 'cpu', 
+                alpha = 0.6, 
+                beta = 0.4, 
+                beta_increment = 0.001):
+        self.tree = Sumtree(buffer_limit)
+
 #Actor
 class ActorClass(nn.Module):
   def __init__(self,
